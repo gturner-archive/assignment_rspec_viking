1435e82b98470d9a29927a8c3a152977e06863e2
Graham Turner <turnertgraham@gmail.com>
2016-07-11 14:58:17 -0400

    completing viking tests

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -4,8 +4,32 @@ require 'weapons/bow'
 
 describe Bow do
   let(:bow){Bow.new}
-  it "should have a bow method" do
 
+  describe "#initialize" do
+    it "should have a arrows method" do
+      expect(bow.arrows).to be_a(Fixnum)
+    end
+
+    it "should return 10 arrows by default" do
+      expect(bow.arrows).to eq(10)
+    end
+
+    it "should allow me to set the number of arrows" do
+      bow_2 = Bow.new(15)
+      expect(bow_2.arrows).to eq(15)
+    end
   end
 
+  describe "#use" do
+    it "reduces the count of arrows by 1" do
+      bow_arrows = bow.arrows
+      bow.use
+      expect(bow.arrows).to eq(bow_arrows - 1)
+    end
+
+    it "raises error if no arrows are left" do
+      empty_shooter = Bow.new(0)
+      expect{empty_shooter.use}.to raise_error('Out of arrows')
+    end
+  end
 end
--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,69 @@
-# Your code here
+require "viking"
+
+describe Viking do
+  let(:vik){Viking.new}
+  describe "#initialize" do
+    it "allows setting viking name" do
+      tom = Viking.new("Tom")
+      expect(tom.name).to eq("Tom")
+    end
+
+    it "allows setting viking health" do
+      tom =  Viking.new("Tom", 90)
+      expect(tom.health).to eq(90)
+    end
+
+    it "does not allow you to set health after initialize" do
+      expect{vik.health = 100}.to raise_error(NoMethodError)
+    end
+
+    it "initilizes viking weapon to nil" do
+      expect(vik.weapon).to eq(nil)
+    end
+  end
+
+  describe "#pick_up_weapon" do
+    let(:fake_wep) do
+      double(is_a?: Weapon)
+    end
+    it "sets the vikings weapons" do
+      vik.pick_up_weapon(fake_wep)
+      expect(vik.weapon).to eq(fake_wep)
+    end
+
+    it "raises an hour if argument is not a weapon" do
+      expect{vik.pick_up_weapon("String")}.to raise_error(RuntimeError)
+    end
+
+    it "replaces the viking's current weapon" do
+      replacement = double(is_a?: Weapon)
+      vik.pick_up_weapon(fake_wep)
+      vik.pick_up_weapon(replacement)
+      expect(vik.weapon).to eq(replacement)
+    end
+  end
+
+  describe "#drop_weapon" do
+    let(:fake_wep) do
+      double(is_a?: Weapon)
+    end
+    it "sets viking weapon to nil" do
+      vik.instance_variable_set(:@weapon, fake_wep)
+      vik.drop_weapon
+      expect(vik.weapon).to be_nil
+    end
+  end
+
+  describe "#receive_attack" do
+    it "reduces health by a set amount" do
+      start = vik.health
+      vik.receive_attack(10)
+      expect(vik.health).to eq(start - 10)
+    end
+
+    it "calls the take_damage method" do
+      expect(vik).to receive(:take_damage)
+      vik.receive_attack(10)
+    end
+  end
+end
